9b859a9a554a02a02641ab9e2b6c62fb9182ab0b
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-23 12:17:55 -0700

    testing slack message interaction

--- app.js
+++ app.js
@@ -18,6 +18,48 @@ app.post('/hello', function(req, res, next){
     text: 'Hello ' + userName + ', welcome to Lg Slack channel! Have fun :) '
   };
 
+app.post('/createfilter', function(req, res, next){
+  var userName = req.body.user_name; 
+  var botPayload= {
+    "text": "Would you like to play a game?",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+  };
+
   if(userName !== 'rentbot' && req.body.channel_name === 'directmessage'){
     return res.status(200).json(botPayload);
   }else{
