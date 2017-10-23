7e4ebbda7ed82ad4b5179244228c393d60959f46
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-23 16:17:35 -0700

    testing slack message interaction

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/sayback', function(req, res, next){
   var callbackId = req.body.callback_id; 
   
   // console.log('Request ', req.body)
-  console.log(req.body.payload)
+  console.log(req.body.payload.user)
 
   var botPayload= {
     text: 'If you are seeing this the route sayback has run.... yay!!!'
