18908d13ae76888d2f17f07a2847f8ba43a0b77d
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-26 11:14:09 -0700

    adding notifications

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var payloads = require('./payloads');
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var axios = require('axios');
 
 var app = express();
 var port = process.env.PORT || 1337;
@@ -14,6 +15,29 @@ app.get('/', function (req, res) {res.status(200).send('Hello World!'); });
 app.listen(port, function(){
   console.log('Listening on port ' + port);
 })
+
+app.post('/api/startscan/:id', function(req, res, next){
+  if(err){
+    return res.send(400).end();
+  }else if(req.params.id !== '153j6kl63hsu38'){
+    return res.status(401).end();
+  }
+  return res.send(200).end();
+})
+
+app.post('/wakeup', function(req, res, next){
+  var userName = req.body.user_name; 
+  var botPayload= {
+    text: 'I\'m up, I\'m up'
+  };
+  console.log('req.body')
+
+if(userName !== 'rentbot' && req.body.channel_name === 'directmessage'){
+  return res.status(200).json(botPayload);
+}else{
+  return res.status(200).end();
+}
+});
 
 app.post('/hello', function(req, res, next){
   var userName = req.body.user_name; 
---
+++ bin/run.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+request('https://rent-finder.herokuapp.com/api/startscan/153j6kl63hsu38', (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(res);
+});
