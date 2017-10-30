bf673d632af48fa979e0c1e3713be08e9c075cab
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-30 15:54:09 -0700

    notification testing in progress

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.post('/sayback', function(req, res, next){
   var userName = payload.user.name
   var botPayload;
 
+  console.log(payload)
   botPayload = dataChecks.createFilterPrompt(payload) 
 
   if(userName !== 'rentbot'){
