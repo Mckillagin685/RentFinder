efd70532ef404b85f59d821bb5bcb231d39289db
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-30 16:10:59 -0700

    notification testing in progress

--- dataChecks.js
+++ dataChecks.js
@@ -4,7 +4,7 @@ var knex = require("knex")
 
 function createFilterPrompt(incomingPayload){
   var callbackId;
-  console.log(incomingPayload)
+  console.log(incomingPayload.callback_id)
   if(incomingPayload.callback_id === "location"){
     callbackId = incomingPayload.callback_id;
   }else{
