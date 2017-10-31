0250bad940ab362c5a5311cfd8845e1680f0cc1f
Lucas Kirsch <grimreaper685@gmail.com>
2017-10-31 12:16:58 -0700

    testing notifictations

--- payloads.js
+++ payloads.js
@@ -515,20 +515,20 @@ var minRentTryAgain = {
   ]
 }
 
-var userNotify = {
-  // "text": "Hey " + user_name + " we found some results for you!!!",
-  "attachments": [
-    {
-      "token" : "dbQN0UHau0QG6deDMzwLzkMg",
-      // "team_id": ,
-      // "team_domain":,
-      // "channel_id":,
-      "channel_name" : "directmessage",
-      // "text": "Here are the results for your filter: " + filter_id + ".",
+// var userNotify = {
+//   // "text": "Hey " + user_name + " we found some results for you!!!",
+//   "attachments": [
+//     {
+//       "token" : "dbQN0UHau0QG6deDMzwLzkMg",
+//       // "team_id": ,
+//       // "team_domain":,
+//       // "channel_id":,
+//       "channel_name" : "directmessage",
+//       // "text": "Here are the results for your filter: " + filter_id + ".",
 
-    }
-  ]
-}
+//     }
+//   ]
+// }
 
 module.exports = {
   location,
