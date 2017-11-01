4bacda0fe571518b149148afd7993b9bfc4cb5fd
Lucas Kirsch <grimreaper685@gmail.com>
2017-11-01 14:22:41 -0700

    testing notifications

--- routes/links.js
+++ routes/links.js
@@ -36,7 +36,7 @@ router.post('/scheduledscraper', (req, res, next) => {
         headers: {
           'Content-type':'json'
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(req.body)
       }
 
       knex('links')
