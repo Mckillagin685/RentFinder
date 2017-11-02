faab37308252fb43d77c4af11d7bf5ff7dd79a05
Lucas Kirsch <grimreaper685@gmail.com>
2017-11-02 08:34:20 -0700

    list filters

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post('/listfilters', function(req, res, next){
     }
     var resbody = JSON.parse(body);
     for (let filter of resbody){
-      console.log(filter)
+      // console.log(filter)
       var field = {
         "title":`Filter ${filter.id}`,
         "value":`Place ${filter.city}, ${filter.state}, Beds: ${filter.beds}, baths: ${filter.baths}, Price range: ${filter.min}-${filter.max}, \n pet friendly:${filter.pet_friendly}, Show only photos:${filter.photo}, notifications on:${filter.notify}`,
