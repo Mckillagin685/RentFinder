ab3bd00033b46343edfefddd9767055612a17915
Lucas Kirsch <grimreaper685@gmail.com>
2017-11-03 11:36:03 -0700

    list results

--- dataChecks.js
+++ dataChecks.js
@@ -171,7 +171,7 @@ function linksToText (links){
   var parsedLinks = JSON.parse(links);
   var result = " ";
   for(let link of parsedLinks){
-    result += `<${link}>, \n \n`
+    result += `${parsedLinks.indexOf(links)}: <${link}>, \n \n`
   }
   console.log(result)
   return result
